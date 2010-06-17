a3fac0ea5dee6654d971d797e1e3768acdc1bfd6
Mikhail Gusarov <dottedmag@dottedmag.net>
2010-06-17 17:01:37 +0700

    Update packaging

--- debian/init.d
+++ debian/init.d
@@ -1,35 +1,29 @@
-#! /bin/sh
+#!/bin/sh
 
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-DESC="unsuspend daemon"
+DESC="autosuspend arbiter"
 NAME="unsuspendd"
-PID="/var/run/unsuspendd.pid"
-
-[ -f /etc/default/unsuspendd ] && . /etc/default/unsuspendd
+PID=/var/run/unsuspendd.pid
 
 case "$1" in
-	start)
-		echo -n "Starting ${DESC}: "
-		start-stop-daemon --start --quiet \
-            --pidfile ${PID} \
-            --exec /usr/sbin/unsuspendd 
-		echo "unsuspendd."
-		;;
-	stop)
-		echo -n "Stopping $DESC: "
-		start-stop-daemon --stop --quiet --pidfile ${PID} --oknodo
-		echo "$NAME."
-		;;
-	restart)
-		$0 stop
-		sleep 1
-		$0 start
-		;;
-	*)
-		N=/etc/init.d/$NAME
-		echo "Usage: $N {start|stop}" >&2
-		exit 1
-	;;
+  start)
+    echo -n "Starting $DESC: "
+    /sbin/start-stop-daemon --start --quiet --pidfile $PID \
+      --exec /usr/sbin/unsuspendd -- -b -s -p
+    echo "$NAME."
+    ;;
+  stop)
+    echo -n "Stopping $DESC: "
+    start-stop-daemon --stop --quiet --pidfile $PID --oknodo
+    echo "$NAME."
+    ;;
+  restart)
+    $0 stop
+    $0 start
+    ;;
+  *)
+    echo "Usage: $NAME {start|stop|restart}" >&2
+    exit 1
+    ;;
 esac
 
 exit 0
